cc6cfadea65abf121795df592e5e3b98ffa94bcc
zfyue <1005319440@qq.com>
2022-08-12 11:15:11 +0800

    Feat demo 工程init

---
+++ custom-spring-boot-starter-demo/src/main/java/cn/zfy/custom/demo/CustomSpringBootStarterDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.zfy.custom.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomSpringBootStarterDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomSpringBootStarterDemoApplication.class, args);
+    }
+
+}
---
+++ custom-spring-boot-starter-demo/src/test/java/cn/zfy/custom/demo/CustomSpringBootStarterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zfy.custom.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomSpringBootStarterDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
